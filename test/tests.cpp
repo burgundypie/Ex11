d02f340cbf9f5d08c780f8cb7d05d563dbc1453b
burgundypie <kotyonok2001@mai.ru>
2021-04-06 17:32:07 +0300

    Симонова Арина

--- test/tests.cpp
+++ test/tests.cpp
@@ -4,4 +4,31 @@
 #include "train.h"
 
 
+TEST(train, test1) 
+{
+	Train train;
+	for (int i = 0; i < 100; ++i) 
+	{
+		train.addCage(true);
+	}
+	EXPECT_EQ(100, train.length());
+}
+
+TEST(train, test2) 
+{
+	Train train;
+	for (int i = 0; i < 1; ++i) 
+	{
+		train.addCage(false);
+	}
+	EXPECT_EQ(1, train.length());
+}
+
+TEST(train, test3) 
+{
+	Cage* cage = new Cage();
+	cage->on();
+	EXPECT_EQ(true, cage->get());
+}
+
 
