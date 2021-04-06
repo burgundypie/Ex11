8ac840d4e20eb1cfcf82765efa10a3ba4b97842d
burgundypie <kotyonok2001@mai.ru>
2021-04-06 17:51:39 +0300

    Симонова Арина

--- include/train.h
+++ include/train.h
@@ -5,32 +5,30 @@
 #include <cstdlib>
 #include <ctime>
 
-class Cage 
-{
+class Cage{
 	bool light;
-public:
-	Cage* next;
-	Cage* prev;
-	Cage() : light(false), next(nullptr), prev(nullptr) {}
-	~Cage();
-	void on() { light = true; }
-	void off() { light = false; }
-	bool get() const { return light; }
-	void setLight(bool l) { light = l; }
-};
+public :
+	Cage * next;
+	Cage * prev;
+	Cage () : light(false), next(nullptr), prev(nullptr) {}
+	~Cage ();
+	void on () { light = true; }
+	void off () { light = false; }
+	bool get () const { return light; }
+	void setLight (bool l) { light = l; }
+} ;
 
-class Train 
-{
+class Train{
 private:
-	Cage* first;
-	Cage* last;
+	Cage * first;
+	Cage * last;
 public:
-	Train() : first(nullptr), last(nullptr) {}
-	void addCage(bool light);
-	void printInfo();
-	size_t length();
-	Cage* getFirst() const { return first; }
-	Cage* getLast() const { return last; }
-};
+	Train () : first(nullptr), last(nullptr) {}
+	void addCage (bool light);
+	void printInfo ();
+	size_t length ();
+	Cage* getFirst () const { return first; }
+	Cage* getLast () const { return last; }
+} ;
 
-#endif
+#endif // INCLUDE_TRAIN_H_
