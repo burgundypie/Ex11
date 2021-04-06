e10d91c4c1c22efeb00f30e0e315555c950ea13d
burgundypie <kotyonok2001@mai.ru>
2021-04-06 17:06:45 +0300

    Симонова Арина

---
+++ include/train.h
@@ -0,0 +1,36 @@
+#ifndef INCLUDE_TRAIN_H_
+#define INCLUDE_TRAIN_H_
+
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+class Cage 
+{
+	bool light;
+public:
+	Cage* next;
+	Cage* prev;
+	Cage() : light(false), next(nullptr), prev(nullptr) {}
+	~Cage();
+	void on() { light = true; }
+	void off() { light = false; }
+	bool get() const { return light; }
+	void setLight(bool l) { light = l; }
+};
+
+class Train 
+{
+private:
+	Cage* first;
+	Cage* last;
+public:
+	Train() : first(nullptr), last(nullptr) {}
+	void addCage(bool light);
+	void printInfo();
+	size_t length();
+	Cage* getFirst() const { return first; }
+	Cage* getLast() const { return last; }
+};
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,22 @@
 
 #include "train.h"
 
-int main() {
+int main() 
+{
+	Train train;
+	srand(time(NULL));
+	for (size_t i = 0; i < std::rand() % 10; ++i)
+	{
+		int temp = std::rand() % 2;
+		if (temp)
+			train.addCage(true);
+		else
+			train.addCage(false);
+	}
+	train.printInfo();
+	std::cout << "Length of the train is " << train.length() << std::endl;
+	std::cout << "After algotithm:" << '\n' << std::endl;
+	train.printInfo();
+	return 0;
   return 0;
 }
-
